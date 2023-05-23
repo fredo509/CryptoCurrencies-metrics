e97ac2c26f29243a74189647263a04d8ef1a00e5
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-23 16:13:08 -0400

    Link home page to home button and as a racine path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/navBar';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navBar';
+import HomeContainer from './components/HomeContainer';
 
 export default function App() {
   return (
-    <NavBar />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeContainer />} />
+        <Route exact path="myprofile" element={<details />} />
+      </Routes>
+    </div>
   );
 }
