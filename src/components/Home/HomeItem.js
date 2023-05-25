faa90d79aed04e6b0c8ee7cf85356585f7a9678f
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-25 17:36:17 -0400

    remove eslint comment

--- src/components/Home/HomeItem.js
+++ src/components/Home/HomeItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// eslint-disable-next-line
 import { FaArrowRight } from 'react-icons/fa';
 
 const HomeItem = (props) => {
